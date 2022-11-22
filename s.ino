034fa7ab767300dbd90645150a51c85741a60576
superstark02 <54995058+superstark02@users.noreply.github.com>
2022-11-22 16:56:19 +0530

    Update s.ino

--- s.ino
+++ s.ino
@@ -1,9 +1,9 @@
 #include <QTRSensors.h>
-#define Kp 0.1 // experiment to determine this, start by something small that just makes your bot follow the line at a slow speed
-#define Kd 4// experiment to determine this, slowly increase the speeds and adjust this value. ( Note: Kp < Kd) 
+#define Kp 0.1 
+#define Kd 1 // ( Note: Kp < Kd) 
 #define MaxSpeed 250
 #define BaseSpeed 150 
-#define speedturn 90
+#define speedturn 100
 #define NUM_SENSORS  6     
 #define rightMotor1 8
 #define rightMotor2 9
@@ -28,6 +28,7 @@ void setup()
   pinMode(leftMotor1, OUTPUT);
   pinMode(leftMotor2, OUTPUT);
   pinMode(leftMotorPWM, OUTPUT);
+  pinMode(buzzer, OUTPUT);
 
   qtr.setTypeAnalog();
   qtr.setSensorPins((const uint8_t[]){A7,A6,A5,A4,A3,A2}, NUM_SENSORS);
@@ -50,11 +51,11 @@ void setup()
     delay(5);
   }
   wait();
-  delay(2000); // wait for 2s to position the bot before entering the main loop 
+  delay(5000); // wait for 2s to position the bot before entering the main loop 
 }  
 
 int lastError = 0;
-unsigned int sensors[8];
+unsigned int sensors[6];
 int position = qtr.readLineWhite(sensors);
 int rightMotorSpeed = 0;
 int leftMotorSpeed = 0;
@@ -62,73 +63,26 @@ int leftMotorSpeed = 0;
 void loop()
 { 
   qtr.readLineWhite(sensors);
-  if(digitalRead(IR_TOP)==0){
-    wait();
-    delay(300);
-    while(digitalRead(IR_RIGHT)==1){
-      move(1, speedturn, 0);//motor derecho hacia adelante
-      move(0, speedturn, 1);//motor derecho hacia adelante
-    }
-    while(digitalRead(IR_RIGHT)==0){
-      move(1, speedturn, 0);//motor derecho hacia adelante
-      move(0, speedturn, 1);//motor derecho hacia adelante
-    }
-    goto pid;
-  }
-  
-  if(sensors[0]&&sensors[1]>700&&sensors[2]>700&&sensors[3]>700&&sensors[4]>700&&sensors[5]){
-    wait();
-    delay(300);
-    while(digitalRead(IR_RIGHT)==1){
-      move(1, speedturn, 0);//motor derecho hacia adelante
-      move(0, speedturn, 1);//motor derecho hacia adelante
-    }
-    while(digitalRead(IR_RIGHT)==0){
-      move(1, speedturn, 0);//motor derecho hacia adelante
-      move(0, speedturn, 1);//motor derecho hacia adelante
-    }
-    goto pid;
-  }
-  
-  if(digitalRead(IR_RIGHT)==0&&digitalRead(IR_LEFT)==0){
-    goto right;
-  }
-  
-  else if(digitalRead(IR_RIGHT)==0){
-    right:
-    while(digitalRead(IR_RIGHT)==0){
-      right();
-    }
-  }
-  
-  else if(digitalRead(IR_LEFT)==0){
-    while(digitalRead(IR_LEFT)==0){
-      left();
-      
-      if(digitalRead(IR_RIGHT)==0){
-        goto right;
-      }
-    }
-    goto pid;
-  }
-  
-  pid:
-    position = qtr.readLineWhite(sensors); 
+  position = qtr.readLineWhite(sensors);
+
+  if(position > 3000 || position < 1700){
+    turn(position);
+  } 
+
+  int error = position - 2500;
+  int motorSpeed = Kp * error + Kd * (error - lastError);
+  lastError = error;
+
+  rightMotorSpeed = BaseSpeed + motorSpeed;
+  leftMotorSpeed = BaseSpeed - motorSpeed;
+
+  if (rightMotorSpeed > MaxSpeed ) rightMotorSpeed = MaxSpeed; 
+  if (leftMotorSpeed > MaxSpeed ) leftMotorSpeed = MaxSpeed;
+  if (rightMotorSpeed < 0)rightMotorSpeed = 0;    
+  if (leftMotorSpeed < 0)leftMotorSpeed = 0;
     
-      int error = position - 2500;
-      int motorSpeed = Kp * error + Kd * (error - lastError);
-      lastError = error;
-    
-      rightMotorSpeed = BaseSpeed + motorSpeed;
-      leftMotorSpeed = BaseSpeed - motorSpeed;
-      
-      if (rightMotorSpeed > MaxSpeed ) rightMotorSpeed = MaxSpeed; 
-      if (leftMotorSpeed > MaxSpeed ) leftMotorSpeed = MaxSpeed;
-      if (rightMotorSpeed < 0)rightMotorSpeed = 0;    
-      if (leftMotorSpeed < 0)leftMotorSpeed = 0;
-        
-      move(1, rightMotorSpeed, 1);//motor derecho hacia adelante
-      move(0, leftMotorSpeed, 1);//motor izquierdo hacia adelante
+  move(1, rightMotorSpeed, 1);//motor derecho hacia adelante
+  move(0, leftMotorSpeed, 1);//motor izquierdo hacia adelante
 }
 
 void wait(){
@@ -141,13 +95,13 @@ void forward(){
 }
 
 void left(){
-      move(1, 0, 1);//motor derecho hacia adelante
+      move(1, speedturn, 1);//motor derecho hacia adelante
       move(0, speedturn, 0);//motor derecho hacia adelante
 }
 
 void right(){
       move(1, speedturn, 0);//motor derecho hacia adelante
-      move(0, 0, 1);//motor derecho hacia adelante
+      move(0, speedturn, 1);//motor derecho hacia adelante
 }
 
 void move(int motor, int speed, int direction){
@@ -174,4 +128,25 @@ void move(int motor, int speed, int direction){
     digitalWrite(rightMotor2, inPin2);
     analogWrite(rightMotorPWM, speed);
   }  
+}
+
+void turn(int cur_position){
+      wait();
+      digitalWrite(buzzer, HIGH);
+      delay(2000);
+      digitalWrite(buzzer, LOW);
+
+      if(cur_position > 3000){
+        qtr.readLineWhite(sensors);
+        while(sensors[2] < 150 && sensors[3] < 150){
+          right();
+        }
+      }
+
+      if(cur_position < 1700){
+        qtr.readLineWhite(sensors);
+        while(sensors[2] < 150 && sensors[3] < 150){
+          left();
+        }
+      }
 }
